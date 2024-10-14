7c096fe26df6e6058c9424b1bb1fab41ef7c1455
ChuTuanVu <chutuanvu020604@gmail.com>
2024-10-14 17:35:10 +0700

    fix bugs

--- myEnglishLab.user.js
+++ myEnglishLab.user.js
@@ -24,6 +24,10 @@
   const activity = document
     .getElementsByClassName("product_design_activity_section")[0]
     ?.textContent.trim();
+  const activity_desc = document
+    .getElementsByClassName("activity-desc__unit-name")[0]
+    ?.textContent.trim();
+
   const fDev = [
     "Exercise 3",
     "Exercise 4A",
@@ -32,7 +36,9 @@
     "Exercise 6A",
     "Exercise 6B",
   ];
-  if (fDev.includes(activity) && !unit) {
+  if (fDev.includes(activity) && !unit && activity_desc == "Reading") {
+    unit = "1.1";
+  } else if (fDev.includes(activity) && !unit) {
     unit = "1.2";
   }
   let answerData = null;
@@ -97,55 +103,57 @@
       } else if (ver == "ta") {
         verta = "https://api.jsonbin.io/v3/b/66ee3797acd3cb34a8885ea5";
       }
-      fetch("https://api.ipify.org/?format=json")
-        .then((response) => response.json())
-        .then((data) => {
-          const ip = data.ip;
-          return fetch(
-            `https://api.airtable.com/v0/appkzsY0wGr47oUqa/MyEnglishLab`,
-            {
-              method: "POST",
-              headers: {
-                Authorization: `Bearer pat6VkxBrIGDyGXKc.5924e5c16e6fc0e6091c563de1e3d8d3df0f75695f3b67af763c6f3f01db5b9c`,
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                records: [
-                  {
-                    fields: {
-                      ip: ip,
-                      time: new Date().toLocaleString(),
+      if (ver && verta) {
+        fetch("https://api.ipify.org/?format=json")
+          .then((response) => response.json())
+          .then((data) => {
+            const ip = data.ip;
+            return fetch(
+              `https://api.airtable.com/v0/appkzsY0wGr47oUqa/MyEnglishLab`,
+              {
+                method: "POST",
+                headers: {
+                  Authorization: `Bearer pat6VkxBrIGDyGXKc.5924e5c16e6fc0e6091c563de1e3d8d3df0f75695f3b67af763c6f3f01db5b9c`,
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  records: [
+                    {
+                      fields: {
+                        ip: ip,
+                        time: new Date().toLocaleString(),
+                      },
                     },
-                  },
-                ],
-              }),
-            }
-          );
-        })
-        .then((response) => response.json())
-        .then(() => {
-          let key = prompt("Liên hệ: chutuanvu0206\nVui lòng nhập key:");
-          if (key) {
-            fetch(verta, {
-              headers: {
-                "X-Access-Key": key,
-              },
-            })
-              .then((response) => {
-                if (!response.ok) {
-                  alert("Key không hợp lệ.");
-                  return;
-                }
-                return response.json();
+                  ],
+                }),
+              }
+            );
+          })
+          .then((response) => response.json())
+          .then(() => {
+            let key = prompt("Liên hệ: chutuanvu0206\nVui lòng nhập key:");
+            if (key) {
+              fetch(verta, {
+                headers: {
+                  "X-Access-Key": key,
+                },
               })
-              .then((data) => {
-                if (data) {
-                  sessionStorage.setItem("cached", JSON.stringify(data));
-                  processActivity(data);
-                }
-              });
-          }
-        });
+                .then((response) => {
+                  if (!response.ok) {
+                    alert("Key không hợp lệ.");
+                    return;
+                  }
+                  return response.json();
+                })
+                .then((data) => {
+                  if (data) {
+                    sessionStorage.setItem("cached", JSON.stringify(data));
+                    processActivity(data);
+                  }
+                });
+            }
+          });
+      }
     }
   });
 
